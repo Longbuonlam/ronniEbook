45deda01227dfc67445af5b75ca54aaacc24662d
Vo Son Long <97535432+Longbuonlam@users.noreply.github.com>
2024-09-08 22:52:08 +0700

    set up favourite book

--- src/main/java/com/ronniebook/web/ebook/domain/FavouriteBook.java
+++ src/main/java/com/ronniebook/web/ebook/domain/FavouriteBook.java
@@ -1,8 +1,10 @@
 package com.ronniebook.web.ebook.domain;
 
+import jakarta.validation.constraints.NotNull;
 import java.io.Serializable;
 import java.time.Instant;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
 
 @Document(collection = "favourite_book")
 public class FavouriteBook extends BaseEntity implements Serializable {
@@ -12,6 +14,12 @@ public class FavouriteBook extends BaseEntity implements Serializable {
     private String bookId;
 
     private Instant addDate;
+
+    @NotNull
+    @Field("book_name")
+    private String bookName;
+
+    private String author;
 
     public FavouriteBook() {}
 
@@ -29,5 +37,21 @@ public class FavouriteBook extends BaseEntity implements Serializable {
 
     public void setAddDate(Instant addDate) {
         this.addDate = addDate;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
     }
 }
--- src/main/java/com/ronniebook/web/ebook/repository/FavouriteBookRepository.java
+++ src/main/java/com/ronniebook/web/ebook/repository/FavouriteBookRepository.java
@@ -1,11 +1,17 @@
 package com.ronniebook.web.ebook.repository;
 
 import com.ronniebook.web.ebook.domain.FavouriteBook;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 /**
  * Spring Data MongoDB repository for the FavouriteBook entity.
  */
 @Repository
-public interface FavouriteBookRepository extends MongoRepository<FavouriteBook, String> {}
+public interface FavouriteBookRepository extends MongoRepository<FavouriteBook, String> {
+    @Query("{'$or':[" + "{ 'book_name' : { $regex: ?0, $options: 'i' } }, " + "{ 'author' : { $regex: ?0, $options: 'i' } }," + "]}")
+    Page<FavouriteBook> findByText(Pageable pageable, String text);
+}
