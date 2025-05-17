8846c2e626d39acc3ecae7a16bc61926260c753c
Long <mac@MacBookPro.lan>
2025-05-18 01:06:14 +0700

    feat: set up user profile for backend

---
+++ src/main/java/com/ronniebook/web/ebook/rest/UserProfileResource.java
@@ -0,0 +1,54 @@
+package com.ronniebook.web.ebook.rest;
+
+import com.ronniebook.web.domain.User;
+import com.ronniebook.web.ebook.domain.dto.UserProfileDTO;
+import com.ronniebook.web.ebook.service.UserProfileService;
+import com.ronniebook.web.security.SecurityUtils;
+import com.ronniebook.web.service.UserService;
+import com.ronniebook.web.web.rest.errors.BadRequestAlertException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/api")
+public class UserProfileResource {
+
+    private static final Logger log = LoggerFactory.getLogger(UserProfileResource.class);
+
+    private final UserProfileService userProfileService;
+
+    private final UserService userService;
+
+    public UserProfileResource(UserProfileService userProfileService, UserService userService) {
+        this.userProfileService = userProfileService;
+        this.userService = userService;
+    }
+
+    @PostMapping("/user-profile/upload-image")
+    public ResponseEntity<User> uploadImage(@RequestParam MultipartFile image) {
+        String userId = SecurityUtils.getCurrentUserLogin().orElseThrow();
+        log.debug("REST request to upload user profile image, user {}", userId);
+        User result = userProfileService
+            .uploadImage(image, userId)
+            .orElseThrow(() -> new BadRequestAlertException("", "User-Profile", "Can not upload image for user profile"));
+        return ResponseEntity.ok().body(result);
+    }
+
+    @PatchMapping("/user-profile/update")
+    public ResponseEntity<User> updateProfile(@RequestBody UserProfileDTO userProfile) {
+        log.debug("REST request to update user profile");
+        User result = userProfileService.updateProfile(userProfile).orElseThrow();
+        return ResponseEntity.ok().body(result);
+    }
+
+    @GetMapping("/user-profile/login-user-info")
+    public ResponseEntity<User> getLoginUserInfo() {
+        log.debug("REST request to get login user info");
+        String login = SecurityUtils.getCurrentUserLogin().orElseThrow();
+        User result = userService.getUserWithAuthoritiesByLogin(login).orElseThrow();
+        return ResponseEntity.ok().body(result);
+    }
+}
---
+++ src/main/java/com/ronniebook/web/ebook/service/UserProfileService.java
@@ -0,0 +1,68 @@
+package com.ronniebook.web.ebook.service;
+
+import com.ronniebook.web.domain.User;
+import com.ronniebook.web.ebook.domain.dto.UserProfileDTO;
+import com.ronniebook.web.repository.UserRepository;
+import com.ronniebook.web.security.SecurityUtils;
+import com.ronniebook.web.web.rest.errors.BadRequestAlertException;
+import java.util.Optional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+public class UserProfileService {
+
+    private final Logger log = LoggerFactory.getLogger(UserProfileService.class);
+    private final UserRepository userRepository;
+    private final CloudinaryService cloudinaryService;
+
+    public UserProfileService(UserRepository userRepository, CloudinaryService cloudinaryService) {
+        this.userRepository = userRepository;
+        this.cloudinaryService = cloudinaryService;
+    }
+
+    public Optional<User> uploadImage(MultipartFile image, String userId) {
+        log.debug("Request to upload image user profile");
+        try {
+            if (image.getSize() > 20 * 1024 * 1024) {
+                throw new BadRequestAlertException("", "User", "File size exceeds the maximum allowed size of 20MB.");
+            }
+
+            String contentType = image.getContentType();
+            if (!cloudinaryService.isImage(contentType)) {
+                throw new BadRequestAlertException("", "User", "Invalid MIME type. Only image files are allowed!");
+            }
+
+            if (!cloudinaryService.hasImageExtension(image.getOriginalFilename())) {
+                throw new BadRequestAlertException("", "User", "Invalid file extension. Only image files are allowed!");
+            }
+            String imageUrl = cloudinaryService.uploadImage(cloudinaryService.convertMultiPartToFile(image));
+
+            User currentUser = userRepository.findOneByLogin(userId).orElseThrow();
+            currentUser.setImageUrl(imageUrl);
+            userRepository.save(currentUser);
+            return Optional.of(currentUser);
+        } catch (Exception e) {
+            throw new BadRequestAlertException("", "", "Error when upload image to cloudinary");
+        }
+    }
+
+    public Optional<User> updateProfile(UserProfileDTO updateUserInfo) {
+        String userId = SecurityUtils.getCurrentUserLogin().orElseThrow();
+        log.debug("Request to update user profile, user {}", userId);
+        User currentUser = userRepository.findOneByLogin(userId).orElseThrow();
+        if (updateUserInfo.getFirstName() != null) {
+            currentUser.setFirstName(updateUserInfo.getFirstName());
+        }
+        if (updateUserInfo.getLastName() != null) {
+            currentUser.setLastName(updateUserInfo.getLastName());
+        }
+        if (updateUserInfo.getEmail() != null) {
+            currentUser.setEmail(updateUserInfo.getEmail());
+        }
+        userRepository.save(currentUser);
+        return Optional.of(currentUser);
+    }
+}
